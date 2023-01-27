7a0fb3b866ab0ec8106ccb6ba59f7f1adc2dda4f
Taco Wannee <76042577+tacoWanneeJama@users.noreply.github.com>
2023-01-27 12:17:40 +0100

    Automapper met Blazorise werkend

--- Automapper2/Client/Program.cs
+++ Automapper2/Client/Program.cs
@@ -13,13 +13,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-//builder.Services
-//    .AddBlazorise(options =>
-//    {
-//        options.Immediate = true;
-//    })
-//    .AddBootstrap5Providers()
-//    .AddFontAwesomeIcons();
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 
 await builder.Build().RunAsync();
