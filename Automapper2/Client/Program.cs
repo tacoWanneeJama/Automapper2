f4e463b6b98711f36081d671c87aa46b54f0f8de
Taco Wannee <76042577+tacoWanneeJama@users.noreply.github.com>
2023-01-27 12:01:18 +0100

    Met mapper

--- Automapper2/Client/Program.cs
+++ Automapper2/Client/Program.cs
@@ -1,11 +1,25 @@
 using Automapper2.Client;
+using Blazorise;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+
 
 await builder.Build().RunAsync();
---
+++ Automapper2/Shared/MapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automapper2.Shared
+{
+    public class MapperProfile: Profile
+    {
+        public MapperProfile() {
+            CreateMap<WeatherForecast, WeatherForecast>();
+        }
+    }
+}
